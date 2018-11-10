edb3b32093f824ca1da287071adf894c7dbd5d96
futpib <futpib@gmail.com>
2018-11-10 19:22:41 +0300

    Add loadModule + unloadModuleByIndex

--- test/e2e/index.js
+++ test/e2e/index.js
@@ -32,29 +32,58 @@ test.serial('connection', async t => {
 test.serial('moveSourceOutput (index, index)', async t => {
   const { pa, connect } = t.context;
   await connect();
+
   const sourceOutputs = await pify(pa).getSourceOutputs();
   const sourceOutput = sourceOutputs.find(so => so.sourceIndex >= 0);
   await pify(pa).moveSourceOutput(sourceOutput.index, sourceOutput.sourceIndex);
+
   t.pass();
 });
 
 test.serial('moveSourceOutput (index, name)', async t => {
   const { pa, connect } = t.context;
   await connect();
+
   const sourceOutputs = await pify(pa).getSourceOutputs();
   const sources = await pify(pa).getSources();
   const sourceOutput = sourceOutputs.find(so => so.sourceIndex >= 0);
   const source = sources.find(s => s.index === sourceOutput.sourceIndex);
   await pify(pa).moveSourceOutput(sourceOutput.index, source.name);
+
   t.pass();
 });
 
 test.serial('setSinkPort (name, name)', async t => {
   const { pa, connect } = t.context;
   await connect();
+
   const sinks = await pify(pa).getSinks();
   const sink = sinks.find(s => s.ports.length > 0);
   const { activePortName } = sink;
   await pify(pa).setSinkPort(sink.name, activePortName);
+
+  t.pass();
+});
+
+test.serial('loadModule + unloadModuleByIndex', async t => {
+  const { pa, connect } = t.context;
+  await connect();
+
+  const modulesBefore = await pify(pa).getModules();
+  await pify(pa).loadModule('module-null-sink', '');
+  const modulesAfter = await pify(pa).getModules();
+
+  t.is(modulesAfter.length, modulesBefore.length + 1);
+
+  const lastModule = modulesAfter[modulesAfter.length - 1];
+
+  t.is(lastModule.name, 'module-null-sink');
+
+  await pify(pa).unloadModuleByIndex(lastModule.index);
+
+  const modulesAfterKill = await pify(pa).getModules();
+
+  t.deepEqual(modulesAfterKill, modulesBefore);
+
   t.pass();
 });
